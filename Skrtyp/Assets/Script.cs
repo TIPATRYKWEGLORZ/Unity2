12b2f4a958b0d136d312423caa2f1dfd24e0f384
TIPATRYKWEGLORZ <91739661+TIPATRYKWEGLORZ@users.noreply.github.com>
2021-10-15 12:17:53 +0200

    Add new files

---
+++ Skrtyp/Assets/Script.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour {
+
+ void Awake () {
+         Debug.Log ("Awake");
+ }
+ 
+ void Start () {
+         Debug.Log ("Start");
+ }
+     void Update () {
+         Debug.Log ("Update: "+Time.deltaTime);
+     } 
+       
+    void FixedUpdate () {
+        Debug.Log ("FixedUpdate: "+Time.deltaTime);
+    }
+}
